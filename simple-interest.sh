6dbb8626ad6954c60fece1ef687dcd75a4705d12
m29i <95764763+m29i@users.noreply.github.com>
2023-08-23 11:15:29 -0700

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # <m29i>
    # Input:
    # p, principal amount
    # t, time period in years
@@ -20,4 +20,3 @@
    s=`expr $p \* $t \* $r / 100`
    echo "The simple interest is: "
    echo $s
-Copied!
